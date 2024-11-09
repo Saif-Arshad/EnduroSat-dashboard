3b8651019ed1c1c60cae2df7f7bae053112b0ec8
Saif Ur Rehman <saifarshad3344@gmail.com>
2024-11-09 18:01:14 +0500

    sownload dependencies

---
+++ auth.config.ts
@@ -0,0 +1,39 @@
+import { NextAuthConfig } from 'next-auth';
+import CredentialProvider from 'next-auth/providers/credentials';
+
+const authConfig = {
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID ?? '',
+    //   clientSecret: process.env.GITHUB_SECRET ?? ''
+    // }),
+    CredentialProvider({
+      credentials: {
+        email: {
+          type: 'email'
+        },
+        password: {
+          type: 'password'
+        }
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: '1',
+          name: 'John',
+          email: credentials?.email as string
+        };
+        if (user) {
+          return user;
+        } else {
+          return null;
+
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '/' //sigin page
+  }
+} satisfies NextAuthConfig;
+
+export default authConfig;
