89f01fd1202da1483e62fdf91e1542d788c1fb37
Saif Ur Rehman <saifarshad3344@gmail.com>
2024-11-17 17:49:01 +0500

    add settlite Info

--- app/dashboard/overview/_components/overview.tsx
+++ app/dashboard/overview/_components/overview.tsx
@@ -21,9 +21,9 @@ export default function OverViewPage() {
   return (
     <PageContainer scrollable>
       <div className="py-5 ">
-        <div className=' grid grid-cols-6 mb-12 gap-x-7'>
+        <div className=' grid grid-cols-1 lg:grid-cols-6 mb-12 gap-x-7'>
 
-          <div className='flex items-center flex-col gap-3 bg-neutral-800 rounded-3xl row-span-2 col-span-2'>
+          <div className='flex items-center flex-col gap-3 bg-neutral-800 rounded-3xl lg:row-span-2 lg:col-span-2'>
             <div className='flex items-center  p-4 rounded-2xl'>
               <img
                 src="/static\Images\sattelite_image.png"
@@ -40,11 +40,64 @@ export default function OverViewPage() {
             </div>
             <GaugeChart />
           </div>
-          <div className='flex items-center flex-col bg-neutral-800 rounded-3xl row-span-4 col-span-4'>
+          <div className='flex items-center flex-col bg-neutral-800 rounded-3xl lg:row-span-4 lg:col-span-4'>
             <LineChart />
             <GaugeChart2 />
           </div>
         </div>
+
+        <div className='bg-neutral-800 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-8 rounded-2xl mb-20 gap-y-10'>
+
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              Space Satellite
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              hct-sat1
+            </h2>
+          </span>
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              Frame Count
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              1322 Frames
+            </h2>
+          </span>
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              Last Frame RCVD at
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              22-11-02 14:52:28
+            </h2>
+          </span>
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              Last Frame RCVD by
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              MAUSYAGI-FN32IG
+            </h2>
+          </span>
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              UpTime
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              1 Week, 1 Day, 11 Hours
+            </h2>
+          </span>
+          <span className='flex items-center flex-col'>
+            <h3 className='text-xl font-semibold capitalize'>
+              OBC Clock
+            </h3>
+            <h2 className='text-3xl font-bold capitalize'>
+              2019-07-26 01:17:33
+            </h2>
+          </span>
+        </div>
+
         <Card />
 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7 my-6">
